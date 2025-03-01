afb9031b85c958c00611f31f9bbc44cd05c326fc
Davide Gerosa <davide.gerosa@gmail.com>
2025-03-01 09:30:49 +0100

    red

---
+++ README.md
@@ -0,0 +1,5 @@
+# scientificcomputing_bicocca
+
+This is a parent repo to handle redirects to the current year's repo. 
+
+[https://dgerosa.github.io/scientificcomputing_bicocca](https://dgerosa.github.io/scientificcomputing_bicocca)
---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+var year = 2024;  // Change this to update the year
+var repoName = "scientificcomputing_bicocca";  // Change this to update the repository name
+var repoBase = "https://github.com/dgerosa/" + repoName + "_" + year;
